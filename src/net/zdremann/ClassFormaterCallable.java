55c598cde7c4f53f73527c6f5aef70762cc2949f
Zachary Dremann <dremann@gmail.com>
2012-05-17 19:04:57 -0400

    Import cleanup

--- src/net/zdremann/ClassFormaterCallable.java
+++ src/net/zdremann/ClassFormaterCallable.java
@@ -10,8 +10,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import net.zdremann.compare.MethodComparator;
 
